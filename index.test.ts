685f2e14a2b54d03fca9bd2c3188072505433ae3
BryceRussell <19967622+BryceRussell@users.noreply.github.com>
2023-03-04 21:07:29 -0600

    Refactor Tests
    - Remove all old tests
    - Test getClosingTags()
    - Test getOpenTags()
    - Test getOpenTagsRaw()
    - Test getAllAttributes() (WIP)

--- index.test.ts
+++ index.test.ts
@@ -1,53 +1,108 @@
-import { describe, expect, it } from 'vitest';
-import { HTMLRx } from './index';
+import { describe, expect, it } from 'vitest'
+import { HTMLRx, getClosingTags, getOpenTags, getOpenTagsRaw, getAllAttributes } from './index'
 
-describe('HTMLRx', () => {
-    const H = new HTMLRx(`
-        <div class="container" data-test="my-container">
-            <h1 class="heading" data-test="my-heading">Heading 1</h1>
-            <p class="paragraph" data-test="my-paragraph">Paragraph 1</p>
-            <ul class="list" data-test="my-list">
-                <li class="item" data-test="my-item-1">Item 1</li>
-                <li class="item" data-test="my-item-2">Item 2</li>
-                <li class="item" data-test="my-item-3">
-                    Item 3
-                    <ol class="nested-list" data-test="my-nested-list">
-                        <li class="nested-item" data-test="my-nested-item-1">Nested item 3.1</li>
-                        <li class="nested-item" data-test="my-nested-item-2">Nested item 3.2</li>
-                    </ol>
-                </li>
+describe('HTMLRx Class', () => {
+  // <!DOCTYPE html>
+  const page = `
+    <html>
+      <head>
+        <meta charset="utf-8">
+        <title>Test Page</title>
+        <link rel="stylesheet" href="styles.css">
+      </head>
+      <body>
+        <header>
+          <nav>
+            <ul>
+              <li><a href="#">Home</a></li>
+              <li><a href="#">About</a></li>
+              <li><a href="#">Contact</a></li>
             </ul>
-        </div>
-    `)
+          </nav>
+        </header>
+        <main>
+          <h1>Welcome to my test page</h1>
+          <p>This is a test page that includes all the basic HTML elements.</p>
+          <form>
+            <label for="name">Name:</label>
+            <input type="text" id="name" name="name" required/>
+            <label for="email">Email:</label>
+            <input type="email" id="email" name="email" required/>
+            <label for="message">Message:</label>
+            <textarea id="message" name="message" required></textarea>
+            <button type="submit">Send</button>
+          </form>
+        </main>
+        <footer>
+          <p>Copyright &copy; 2023</p>
+        </footer>
+      </body>
+    </html>
+  `
 
-    it('select()', () => {
+  const HTMLTests = [
+    '<html></html>',
+    '<html lang="en"></html>',
+    '<details open></details>',
+    '<html><body></body></html>',
+    '<html lang="en"><body class="dark" data-theme="dark">Descriptive Text</body></html>',
+    page
+  ]
 
-        expect(H.select('ol')).toStrictEqual([['<ol class="nested-list" data-test="my-nested-list">', 489]])
-        expect(H.select('h1',{class:'heading'})).toStrictEqual([['<h1 class="heading" data-test="my-heading">', 70]])
-        expect(H.select('li',{'data-test':'my-item-2'})).toStrictEqual([['<li class="item" data-test="my-item-2">', 335]])
-        expect(H.select('li',{'':'item'})).toStrictEqual([['<li class="item" data-test="my-item-1">', 268]])
-        expect(H.select('li',{'':'item'}, 2)).toStrictEqual([
-            ['<li class="item" data-test="my-item-1">', 268],
-            ['<li class="item" data-test="my-item-2">', 335]
-        ])
-        expect(H.select('li',{'':'item'}, true)).toStrictEqual([
-            ['<li class="item" data-test="my-item-1">', 268],
-            ['<li class="item" data-test="my-item-2">', 335],
-            ['<li class="item" data-test="my-item-3">', 402]
-        ])
-        // expect(H.select(null,{'': 'my-container'})).toStrictEqual(['<div class="container" data-test="my-container">'])
-    });
+  const H = new HTMLRx(page)
 
-    it('element()', () => {
+  it('Get closing tags', () => {
+    for (const test of HTMLTests) {
+      const tags = getClosingTags(test)
+      for (const [tag, indexes] of tags) {
+        for (const i of indexes) {
+          expect(test.slice(i - tag.length - 3, i)).toBe(`</${tag}>`)
+        }
+      }
+    }
+  })
 
-        expect(H.element('ol')).toBe(`<ol class="nested-list" data-test="my-nested-list">
-                        <li class="nested-item" data-test="my-nested-item-1">Nested item 3.1</li>
-                        <li class="nested-item" data-test="my-nested-item-2">Nested item 3.2</li>
-                    </ol>`)
+  it('Get open tags', () => {
+    for (const test of HTMLTests) {
+      const tags = getOpenTags(test)
+      for (const [tag, indexes] of tags) {
+        for (const i of indexes) {
+          expect(test.slice(i, i + 1 + tag.length)).toBe(`<${tag}`)
+        }
+      }
+    }
+  })
 
-        expect(H.element('h1',{class:'heading'})).toBe('<h1 class="heading" data-test="my-heading">Heading 1</h1>')
-        // expect(T.getElement('li',{'data-test':'my-item-2'})).toBe('<li class="item" data-test="my-item-2">')
-        // expect(T.getElement('li',{'':'item'})).toBe('<li class="item" data-test="my-item-1">')
-        // expect(T.getElement(null,{'': 'my-container'})).toBe('<div class="container" data-test="my-container">')
-    });
-});
+
+  it('Get raw tags', () => {
+    for (const test of HTMLTests) {
+      const tags = getOpenTagsRaw(test)
+      for (const [tag, indexes] of tags) {
+        for (const i of indexes) {
+          expect(test.slice(i, i + tag.length)).toBe(tag)
+        }
+      }
+    }
+  })
+
+  // it('Get attributes from raw tags', () => {
+  //   for (const test of HTMLTests) {
+  //     const openTags = getOpenTagsRaw(test)
+  //     const rawTags = getAllAttributes(openTags)
+  //     for (const [tag, attrs] of rawTags.entries()) {
+  //       const hasTag = test.includes(tag)
+  //       for (const [attr, val] of Object.entries(attrs)) {
+  //         const hasAttrs = 
+  //           (tag.includes(`${attr} `)) ||
+  //           (tag.includes(` ${attr} `)) ||
+  //           (tag.includes(` ${attr}`)) ||
+  //           (tag.includes(`${attr}="${val}"`)) ||
+  //           (tag.includes(`${attr}='${val}'`)) ||
+  //           (tag.includes(`${attr}=${val}`))
+  //           console.log('HEREEER: ', hasTag, hasAttrs)
+  //         expect(hasTag && hasAttrs).toBe(true)
+  //       }
+  //     }
+  //   }
+  // })
+})
